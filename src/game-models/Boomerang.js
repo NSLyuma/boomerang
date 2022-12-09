afe7d19e98fb903bbf5c48ad3836c0d25c3a451f
Anastasiya <iinokayo@mail.ru>
2022-12-09 16:28:40 +0300

    Fixed running boomerang throw enemy

--- src/game-models/Boomerang.js
+++ src/game-models/Boomerang.js
@@ -8,32 +8,32 @@ class Boomerang {
     this.position = position;
   }
 
-//   fly(heroPosition, enemyPosition) {
-//     let timer = 0;
-//     setInterval(() => {
-//       if (timer < 10 && this.position < enemyPosition) {
-//         this.moveRight();
-//         timer += 1;
-//       } else if (
-//         timer >= 10 &&
-//         timer < 20 &&
-//         this.position > heroPosition + 1
-//       ) {
-//         this.moveLeft();
-//         timer += 1;
-//       }
-//     }, 50);
-//   }
+  // fly(heroPosition, enemyPosition) {
+  //   let timer = 0;
+  //   setInterval(() => {
+  //     if (timer < 10 && this.position < enemyPosition) {
+  //       this.position += 1;
+  //       timer += 1;
+  //     } else if (
+  //       timer >= 10 &&
+  //       timer < 20 &&
+  //       this.position > heroPosition + 1
+  //     ) {
+  //       this.position -= 1;
+  //       timer += 1;
+  //     }
+  //   }, 50);
+  // }
 
-//   moveLeft() {
-//     // Идём влево.
-//     this.position -= 1;
-//   }
+  moveLeft() {
+    // Идём влево.
+    this.position -= 1;
+  }
 
-//   moveRight() {
-//     // Идём вправо.
-//     this.position += 1;
-//   }
- }
+  moveRight() {
+    // Идём вправо.
+    this.position += 1;
+  }
+}
 
 module.exports = Boomerang;
