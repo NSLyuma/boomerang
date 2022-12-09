c488befd234ccd86093c7c65b20937aa7b277401
Anastasiya <iinokayo@mail.ru>
2022-12-09 15:02:28 +0300

    Adding player name

--- src/View.js
+++ src/View.js
@@ -1,9 +1,13 @@
+const playerName = process.argv[2] || 'DEFAULT PLAYER 👽';
+
 class View {
   render(track) {
     const yourTeamName = '🚀🚀🚀🚀';
 
     // Тут всё рисуем.
     console.clear();
+    console.log(`Player: ${playerName}`);
+    console.log();
     console.log(track.join(''));
     console.log('\n\n');
     console.log(`Created by ${yourTeamName} with love`);
